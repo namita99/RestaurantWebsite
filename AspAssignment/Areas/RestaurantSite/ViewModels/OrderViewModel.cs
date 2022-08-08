5d5de34ee68dd2f1f9822794ba8f94c86ac650f1
Namita <namita@simplilearn.com>
2022-08-08 23:27:54 +0530

    Changed Layouts

---
+++ AspAssignment/Areas/RestaurantSite/ViewModels/OrderViewModel.cs
@@ -0,0 +1,82 @@
+﻿using AspAssignment.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspAssignment.Areas.RestaurantSite.ViewModels
+{
+    public class OrderViewModel:Order
+    {
+        [Display(Name = "Order ID")]
+        override public int OrderId
+        {
+            get
+            {
+                return base.DishId;
+            }
+            set
+            {
+                base.DishId = value;
+            }
+        }
+
+        [Display(Name ="Order Status")]
+        [Required(ErrorMessage = "{0} cannot be Empty!")]
+        [MinLength(3, ErrorMessage = "{0} should have at least {1} characters")]
+        [MaxLength(100, ErrorMessage = "{0} should have maximum {1} characters")]
+
+        override public string OrderStatus
+        {
+            get => base.OrderStatus;
+            set => base.OrderStatus = value;
+        }
+
+        [Display(Name = "Quantity")]
+        override public short Quantity
+        {
+            get => base.Quantity;
+            set => base.Quantity = value;
+        }
+
+        [Display(Name = "Category Name")]
+        override public int CategoryId
+        {
+            get
+            {
+                return base.CategoryId;
+            }
+            set
+            {
+                base.CategoryId = value;
+            }
+        }
+
+        [Display(Name = "Dish Name")]
+        override public int DishId
+        {
+            get
+            {
+                return base.DishId;
+            }
+            set
+            {
+                base.DishId = value;
+            }
+        }
+
+        [Display(Name = "Customer Name")]
+        override public int CustomerId
+        {
+            get
+            {
+                return base.CustomerId;
+            }
+            set
+            {
+                base.CustomerId = value;
+            }
+        }
+
+
+
+
+    }
+}
--- AspAssignment/Models/Order.cs
+++ AspAssignment/Models/Order.cs
@@ -10,22 +10,22 @@ namespace AspAssignment.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Display(Name = "Order ID")]
-        public int OrderId { get; set; }
+        virtual public int OrderId { get; set; }
 
 
         [Required(ErrorMessage = "{0} cannot be Empty!")]
         [MinLength(4, ErrorMessage = "{0} should have at least {1} characters")]
         [MaxLength(100, ErrorMessage = "{0} should have maximum {1} characters")]
         [Display(Name = "Order Status")]
-        public string OrderStatus { get; set; }
+        virtual public string OrderStatus { get; set; }
 
         [Required]
         [DefaultValue(1)]
-        public short Quantity { get; set; }
+        virtual public short Quantity { get; set; }
 
         #region Navigation Properties to the Category Model
 
-        public int CategoryId { get; set; }
+        virtual public int CategoryId { get; set; }
 
 
         [ForeignKey(nameof(Order.CategoryId))]
@@ -33,13 +33,13 @@ namespace AspAssignment.Models
      
         public Category Category { get; set; }
 
-        public int DishId { get; set; }
+       virtual public int DishId { get; set; }
 
 
         [ForeignKey(nameof(Order.DishId))]
         public Menu Menu { get; set; }
 
-        public int CustomerId { get; set; }
+       virtual public int CustomerId { get; set; }
 
 
         [ForeignKey(nameof(Order.CustomerId))]
